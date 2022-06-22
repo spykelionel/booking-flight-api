c1ff4617808adb4affa7dcb124a40e80449a5e48
spykelion <ndilionel6@gmail.com>
2022-06-22 12:32:32 +0100

    Define fligth routes

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router.get('/', controller.getAllFlight)
+router.post('/', controller.addFlight)
+router.get('/:id', controller.getSingleFlight)
+router.patch('/:id', controller.updateFlight)
+router.delete('/:id', controller.deleteFlight)
 
 module.exports = router;
 
